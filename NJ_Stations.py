5727c9a84fdad8a4cd6da16ac38c5a94f5d6dfbc
Logan Soldo <lsoldo@pm.me>
2020-11-25 11:26:38 -0500

    Add files via upload

--- NJ_Stations.py
+++ NJ_Stations.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 """
 Creating a program to compare datasets and fill in...
 
@@ -16,39 +15,76 @@ import math
 from scipy.stats import linregress
 from scipy.stats import stats
 
-def read_file(station,station_fill):
+def read_file(station,method):
     print(station)
     while True:
         try:
-            data = pd.read_csv('E:\School\RutgersWork\DEP_Precip\Stations\%s.csv'%(station),parse_dates=['DATE'],infer_datetime_format=True,na_values=[" "," S"," T"," M"]).fillna(0)
-            print(data)
-     #       data = data.replace('S',0)
+            if method == "main":
+                data = pd.read_csv('E:\School\RutgersWork\DEP_Precip\Stations\MainData\%s.csv'%(station),parse_dates=['DATE'],infer_datetime_format=True,na_values=[" "," S"," T"," M"])
+            elif method =="fill":
+                data = pd.read_csv('E:\School\RutgersWork\DEP_Precip\Stations\Fill_In\%s.csv'%(station),parse_dates=['DATE'],infer_datetime_format=True,na_values=[" "," S"," T"," M"])
 
         except:
             print("Problem Finding File. Exiting")
             break
         
         data['PRCP'] = data['PRCP'].astype(str)
-   #     data = data.PRCP.str.extract(r'(?P<PRCP>[0-9.0-9]*)(?P<STR>[A]{0,1})',expand=True)
         data['PRCP'] =  data['PRCP'].str.extract('([+-.0-9]+)(.*)').rename(columns={0:'PRCP', 1:'UNIT'})
 
         data['PRCP'] = pd.to_numeric(data.PRCP,errors='coerce')
         data = data.set_index(['DATE'])
-     #   data = data.loc['1950-01-01':'2019-12-31']        # 70 year period of record...
         data = data.loc['1900-01-01':'2019-12-31']        # Extended long term period of record...
         data.reset_index(level=0,inplace=True)
-        print(data[['DATE','PRCP']])
-        return data[['DATE','PRCP']]
+   #     print(data)
+        return data
 
+def fill_file(station_list):
+    main_file = read_file(station_list[0],"main")
+    main_file = main_file.set_index('DATE')
+    temp = pd.date_range(start = '1900-01-01',end='2019-12-31')
+    main_file = main_file.reindex(temp)
+#    print(main_file)
+    if len(station_list) > 1:
+        fill_file = read_file(station_list[1],"fill")
+        fill_file = fill_file.set_index('DATE')
+        temp = pd.date_range(start = '1900-01-01',end='2019-12-31')        
+        fill_file = fill_file.reindex(temp)
+     #   print(fill_file)
+        
+        mask = main_file['PRCP'].isnull()
+        main_file.loc[mask,:] = fill_file.loc[mask,:]
+ 
+    main_file = main_file.reset_index()
+    main_file.columns = ['DATE','NAME','PRCP']
+    main_file = main_file.set_index('DATE')
+   # print(main_file)
+    
+    save_csv(main_file,station_list[0])
+
+    main_file['counter'] = np.where(main_file['PRCP'].isnull(),1,0)
+#    main_file['filled'] = np.where(main_file['NAME'])
+    print(main_file)
+    miss = main_file['counter'].sum()
+ #   print(station_list[0],miss)
+    return station_list[0],miss
+    
+
+def save_csv(df,station):
+    df.to_csv('E:\School\RutgersWork\DEP_Precip\Stations\%s.csv'%station)
 def main():
 #    station_list = ['New Brunswick']
     station_list = [['Coastal South','Atlantic City'],
                     ['Northwest','Newton'],
-                    ['Central'],
+                    ['Central','New Brunswick'],
                     ['Northeast','Dover'],
-                    ['Coastal North'],
-                    ['Southwest']]    
-#    station_list = ['Northeast']
-    
+                    ['Coastal North','New Brunswick'],
+                    ['Southwest','Pemberton']]    
+  #  station_list = [['Coastal South','Atlantic City']]
+    stats=[]
     for station in station_list:
-        data = read_file(station)
+        main_station,data = fill_file(station)
+        stats.append([main_station,data])
+    print(stats)
+
+if __name__ == "__main__":
+    main()  
